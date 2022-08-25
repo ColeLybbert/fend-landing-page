07cf770a2ca1570b6191b3d04c1baf61d4a5a180
Cole Lybbert <cole.lybbert65@gmail.com>
2022-08-25 03:57:10 -0700

    added readme file

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-Landing-Page:
+# Landing Page
 
-This is a project is supposed to test my basic JavaScript skills
-its a simple webpage with pretty basic styling and some cool JavaScript.
-
-Get started:
-
-There are no dependencies for this project, it can be found at https://github.com/ColeLybbert/landing-page. 
-
-Running Project:
-
-Copy the file and git clone the file in the terminal and it should run properly.
+This is a project that was made to test my JavaScript Skills, there is a lack of design but the functionality is all there in the JavaScript Page.
--- js/app.js
+++ js/app.js
@@ -1,26 +1,33 @@
-let navItems = ['Function','Disease','Memories','Restoration'];
-let navList = document.querySelector('#nav');
+let navItems = ["Function", "Disease", "Memories", "Restoration"];
+let navList = document.querySelector("#nav");
 
-navItems.map(navItem=>{
-    let listItem = document.createElement('li');
-    let link = document.createElement('a');
-    link.href = '#'+navItem;
-    
-    link.innerHTML = navItem;
-    
-    listItem.appendChild(link);
-    navList.appendChild(listItem);
+navItems.map((navItem) => {
+  let listItem = document.createElement("li");
+  let link = document.createElement("a");
+
+  link.href = "#" + navItem;
+  listItem.id = navItem;
+  link.innerHTML = navItem;
+
+  listItem.appendChild(link);
+  navList.appendChild(listItem);
+});
+
+let sections = [].slice.call(document.getElementsByClassName("section"));
+
+sections.map((section) => {
+  let matchingButton = document.querySelector('#'+section.dataset.id);
+  section.addEventListener("mouseover", () => {
+    section.classList.add("active");
+    matchingButton.classList.add("selected");
+  });
+
+  section.addEventListener("mouseout", () => {
+    section.classList.remove("active");
+    matchingButton.classList.remove("selected");
+
+  });
 });
 
 
-let sections = [].slice.call(document.getElementsByClassName('section'));
-    sections.map(section=>{
-        section.addEventListener("mouseover",()=>{
-            section.classList.add('active');
-        })
-    });
-    sections.map(section=>{
-        section.addEventListener("mouseout",()=>{
-            section.classList.remove('active');
-        })
-    });
+
