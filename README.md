1c43eab78556e2528631ec976809a2f1968e260d
Cole Lybbert <cole.lybbert65@gmail.com>
2022-09-19 10:39:50 -0700

    Changed mouseover to scroll

--- README.md
+++ README.md
@@ -4,4 +4,16 @@ This is a project that was made to test my JavaScript Skills, there is a lack of
 
 # JavaScript
 
-Using .slice and .call you can put everything with the tag "section" into and array, once it is in an array you can iterate it and using .map I created the basic elements for the nav bar, "li" and "a". Using innerHTML I labeled each "a" with the sections dataset-id's, then added a link to the "a" for the scroll. I attached the "a" tag to the "li" tag, then attached the "li" tag to the NavBar, that is how the NavBar was created. Next is is the hover effects for both the section and it's unique NavBar item. We are still iterating with section so all we have to do is add and Event Listener "mouseover" with a Anonymous Function. Inside the function I added a class to each section as well as it's item when hovered over. Then using another Event Listener and Anonymous Function I did the same thing, exept removing both classes on "mouseout".
+ Line 1-3: Defining every Section and the Nav Bar.
+
+ Line 5-14: Creating a 'li' and 'a' giving them an id, an href, and attaching them to Nav Bar.
+
+ Line 16-19: Defining each individual section.
+
+ Line 21-24: Defining when the section begins on the screen.
+
+ Line 26-29: Defining when the section ends on the screen.
+
+ Line 31-34: Defining each Individual Nav Item.
+ 
+ Line 36-62: Using and Event Listener and If Else statements to decide if the screen is over any of the Sections and if so adding classes.
--- js/app.js
+++ js/app.js
@@ -1,30 +1,66 @@
-//Returning section into an array
-let sections = [].slice.call(document.getElementsByTagName("section"));
-// Defining NavBar
-let navBar = document.getElementById("navbar__list");
-// Looping each section and nav button
-sections.map((section) => {
-  // Creating NavBar Structure
+//define sections and navbar
+let sections = document.querySelectorAll('section');
+let navBar = document.getElementById('navbar__list');
+
+// create nav items
+sections.forEach((section) => {
   let navItem = document.createElement('li');
   let itemLink = document.createElement('a');
-  //Adding link for scroll on click
-  itemLink.href = "#" + section.dataset.scroll;
-  //Labeling each Nav Button
+  navItem.id = "nav" + section.id;
+  itemLink.href = "#" + section.id;
   itemLink.innerHTML = section.dataset.id;
-  //Attaching everything together
   navItem.appendChild(itemLink);
   navBar.appendChild(navItem);
-  //Adding Classes
-  section.addEventListener("mouseover", () => {
-    section.classList.add("your-active-class");
-    navItem.classList.add("nav-active-class");
-  });
-  //Removing Classes
-  section.addEventListener("mouseout", () => {
-    section.classList.remove("your-active-class");
-    navItem.classList.remove("nav-active-class");
-  });
 });
+
+// define each section
+let section1 = document.getElementById('section1');
+let section2 = document.getElementById('section2');
+let section3 = document.getElementById('section3');
+
+//define top of section for scroll
+let section1Top = section1.offsetTop -500;
+let section2Top = section2.offsetTop -500;
+let section3Top = section3.offsetTop -500;
+
+// define bottom of sections for scroll
+let section1Bottom = section1.offsetTop + section1.offsetHeight -500;
+let section2Bottom = section2.offsetTop + section2.offsetHeight -500;
+let section3Bottom = section3.offsetTop + section3.offsetHeight -500;
+
+// define nav items
+let nav1 = document.getElementById('navsection1');
+let nav2 = document.getElementById('navsection2');
+let nav3 = document.getElementById('navsection3');
+
+// add event listener using top and bottom of sections to add classes
+window.addEventListener('scroll', () => {
+  let scrolled = window.scrollY;
+  if(scrolled >= section1Top & scrolled <= section1Bottom) {
+    section1.classList.add('your-active-class');
+    nav1.classList.add('nav-active-class');
+  } else {
+    section1.classList.remove('your-active-class');
+    nav1.classList.remove('nav-active-class');
+  }
+
+  if(scrolled >= section2Top & scrolled <= section2Bottom) {
+    section2.classList.add('your-active-class');
+    nav2.classList.add('nav-active-class');
+  } else {
+    section2.classList.remove('your-active-class');
+    nav2.classList.remove('nav-active-class');
+  }
+
+  if(scrolled >= section3Top & scrolled <= section3Bottom) {
+    section3.classList.add('your-active-class');
+    nav3.classList.add('nav-active-class');
+  } else {
+    section3.classList.remove('your-active-class');
+    nav3.classList.remove('nav-active-class');
+  }
+});
+    
 
 
 
