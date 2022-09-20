1efbb8612156245f9b6d590b6ce779243e399864
Cole Lybbert <cole.lybbert65@gmail.com>
2022-09-20 14:29:29 -0700

    Made code not static

--- README.md
+++ README.md
@@ -8,12 +8,6 @@ This is a project that was made to test my JavaScript Skills, there is a lack of
 
  Line 5-14: Creating a 'li' and 'a' giving them an id, an href, and attaching them to Nav Bar.
 
- Line 16-19: Defining each individual section.
-
- Line 21-24: Defining when the section begins on the screen.
-
- Line 26-29: Defining when the section ends on the screen.
-
- Line 31-34: Defining each Individual Nav Item.
+ Line 16-19: Defining each section top and bottom for scroll listener
  
- Line 36-62: Using and Event Listener and If Else statements to decide if the screen is over any of the Sections and if so adding classes.
+ Line 36-62: Using and Event Listener and If Else statement to decide if the screen is over any of the Sections and if so adding classes. If the screen isn't over the sections it removes the classes.
--- js/app.js
+++ js/app.js
@@ -11,54 +11,22 @@ sections.forEach((section) => {
   itemLink.innerHTML = section.dataset.id;
   navItem.appendChild(itemLink);
   navBar.appendChild(navItem);
-});
 
-// define each section
-let section1 = document.getElementById('section1');
-let section2 = document.getElementById('section2');
-let section3 = document.getElementById('section3');
+  //define section top and bottom
+  let sectionTop = section.offsetTop -500;
+  let sectionBottom = section.offsetTop + section.offsetHeight - 500;
 
-//define top of section for scroll
-let section1Top = section1.offsetTop -500;
-let section2Top = section2.offsetTop -500;
-let section3Top = section3.offsetTop -500;
-
-// define bottom of sections for scroll
-let section1Bottom = section1.offsetTop + section1.offsetHeight -500;
-let section2Bottom = section2.offsetTop + section2.offsetHeight -500;
-let section3Bottom = section3.offsetTop + section3.offsetHeight -500;
-
-// define nav items
-let nav1 = document.getElementById('navsection1');
-let nav2 = document.getElementById('navsection2');
-let nav3 = document.getElementById('navsection3');
-
-// add event listener using top and bottom of sections to add classes
-window.addEventListener('scroll', () => {
-  let scrolled = window.scrollY;
-  if(scrolled >= section1Top & scrolled <= section1Bottom) {
-    section1.classList.add('your-active-class');
-    nav1.classList.add('nav-active-class');
-  } else {
-    section1.classList.remove('your-active-class');
-    nav1.classList.remove('nav-active-class');
-  }
-
-  if(scrolled >= section2Top & scrolled <= section2Bottom) {
-    section2.classList.add('your-active-class');
-    nav2.classList.add('nav-active-class');
-  } else {
-    section2.classList.remove('your-active-class');
-    nav2.classList.remove('nav-active-class');
-  }
-
-  if(scrolled >= section3Top & scrolled <= section3Bottom) {
-    section3.classList.add('your-active-class');
-    nav3.classList.add('nav-active-class');
-  } else {
-    section3.classList.remove('your-active-class');
-    nav3.classList.remove('nav-active-class');
-  }
+  //add scroll event listener and add and remove classes
+  window.addEventListener('scroll', () => {
+    let scrolled = window.scrollY;
+    if (scrolled >= sectionTop & scrolled <= sectionBottom) {
+      section.classList.add('your-active-class');
+      navItem.classList.add('nav-active-class');
+    } else {
+      section.classList.remove('your-active-class');
+      navItem.classList.remove('nav-active-class');
+    }
+  })
 });
     
 
